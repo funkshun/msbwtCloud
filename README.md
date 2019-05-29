b937f52e91111113033b20172c12903577fc579f
Pyrrxa <pyrrxa@gmail.com>
2019-05-29 12:18:25 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,26 +1,103 @@
-## VM Startup
-1. Create a new VM instance. We recommend a machine type of 2 virtual CPUs (vCPUs) with 52 GB memory, an Ubuntu 14.04 LTS OS, 
-and SSD disk of 48 GB (dependent on the size of your msbwt, as a general rule add 10GB of memory to the size of your msbwt).
-2. [**Only needs to be done ONCE per project**] Add a Firewall rule in Google Compute Engine to allow traffic to port 8080. Within the GPC dashboard, navigate to Networking --> VPC network --> Firewall rules. Add an ingress rule that filters on all IP ranges (`0.0.0.0/0`) and the 8080 port (`tcp:8080`).
-  - Name you instance and provide a description
-  - Network: default
-  - Priority: 1000
-  - Direction of traffic: Ingress
-  - Action on match: Allow
-  - Targets: All instances in the network
-  - Source filter: IP ranges
-  - Source IP ranges: `0.0.0.0/0`
-  - Second source filter: None
-  - Protocols and ports: Specified protocols and ports, `tcp:8080`
-3. SSH into the VM.
-4. Pull down the msbwtCloud startup script via: 
+# msBWT Server and Cloud
+
+
+## msBWT RESTful interface and Name Resolution
+
+A paired server combination implemented with Flask. msbwtCloud provides an RESTful interface for non-blocking requests to a remote msBWT data structure. Queries return a token and summary and results can be retrieved by providing the token. msbwtServer provides name resolution and management for multiple remote msBWT structures including status checking, dynamic response to resource movement, and a basic web interface.
+
+## Installation 
+- Clone this repository on to the device hosting the BWT datastructure.
+- TODO
+
+## msBWTCloud
+
+msBWTCloud provides the primary interface for interacting with remote BWT data structures. The following functions are exposed at:
 ```
-wget https://raw.githubusercontent.com/mnajarian/msbwtCloud/master/startup.sh
+    /<function_name>?args=[args_list]
 ```
-5. Run the startup script via: ```source startup.sh```. Navigate through the prompts. When prompted, enter a host server and msbwt location. For example:
+
+### countOccurrencesOfSeq
+Queries the number of times a sequence occurrs in the target dataset.
+
+- Arguments  
+    `'seq'`: the target sequence to be counted
+- Sample Call  
+`http://test.test/countOccurrencesOfSeq?args=['CATAGAT']`  
+Queries the number of occurrences of `CATAGAT` in the dataset held by `test.test`
+ 
+### Return Structure
+The functions above do not directly return their results to prevent blocking on long running calls. Instead, a token is given that can be used to retrieve the status of a query.  
+#### Sample Return JSON for countOccurrencesOfSeq
 ```
-mnajarian@csbio-desktop008.cs.unc.edu:/csbiodata/perlegen/CC_bwt/CC001M4363_UNC_NYGC/
+{
+    "function": "countOccurrencesOfSeq",
+    "token"   : "SqFg2Pjrko8qhFz",
+    "args"    : ['CATAGAT'],
+    "kwargs"  : {},
+    "data"    :{
+                "name"        : "CC027M756_UNC_NYGC",
+                "description" : "Collaborative Cross Dataset 27 Male"
+                "load"        : 4
+    }
+}
 ```
-6. Navigate to the msbwtCloud directory at ```cd ~/msbwtCloud```
-7. (Optional) Create a logs directory
-8. Start the server: ```nohup python core.py /playpen/[msbwtName] > logs/[logFile] & ```
+
+## Obtaining Results
+The status of a query can be obtained using the token value returned by the above functions. The token is passed to the url:  
+```
+/results/<token>
+```
+#### Sample Results JSON for Running Query
+```
+{
+    "date"    : "01/01/1990, 00:00:00",
+    "function : "countOccurrencesOfSeq",
+    "args"    : ['CATAGAT']
+    "kwargs"  : {}
+    "status"  : "RUNNING"
+    "result"  : null,
+    "data"    :{
+                "name"        : "CC027M756_UNC_NYGC",
+                "description" : "Collaborative Cross Dataset 27 Male"
+                "load"        : 4
+    }
+}
+```
+#### Sample Results JSON for Successful Quey
+```
+{
+    "date"    : "01/01/1990, 00:00:00",
+    "function : "countOccurrencesOfSeq",
+    "args"    : ['CATAGAT']
+    "kwargs"  : {}
+    "status"  : "SUCCESS"
+    "result"  : 9327856,
+    "data"    :{
+                "name"        : "CC027M756_UNC_NYGC",
+                "description" : "Collaborative Cross Dataset 27 Male"
+                "load"        : 4
+    }
+}
+```
+
+#### Sample Results JSON for Successful Quey
+```
+{
+    "date"    : "01/01/1990, 00:00:00",
+    "function : "countOccurrencesOfSeq",
+    "args"    : ['CATAGAT']
+    "kwargs"  : {}
+    "status"  : "FAILED"
+    "result"  : "ValueError: countOccurrencesOfSeq takes exactly one argument",
+    "data"    :{
+                "name"        : "CC027M756_UNC_NYGC",
+                "description" : "Collaborative Cross Dataset 27 Male"
+                "load"        : 4
+    }
+}
+```
+
+
+
+
+
--- msbwtCloud/__init__.py
+++ msbwtCloud/__init__.py
@@ -13,9 +13,10 @@ from flask import Response
 from flask import request
 from flask import render_template
 from apscheduler.schedulers.background import BackgroundScheduler
-from multiprocessing.pool import ThreadPool
+#from multiprocessing.pool import ThreadPool
 from threading import Thread
 from MUSCython import MultiStringBWTCython as MSBWT
+from fastBatchKmerCounter import generate_counts as fastBatchKmerCounts
 
 
 def create_app(test_config=None):
@@ -54,13 +55,17 @@ def create_app(test_config=None):
 
     
     results_lst = {}
-    pool = ThreadPool(processes=2)
+    #pool = ThreadPool(processes=2)
     
-    
-    
+    """
+        Primary Function Route
+        Accepts connections bearing the name of a functioon and the corresponding
+        arguments in the form ?args=[...]
+        Accepted Functions:
+        -countOccurrencesOfSeq
+        -recoverString
 
-    
-
+    """
     @app.route('/<func_call>')
     def functionCaller(func_call):
         if func_call == 'checkAlive':
@@ -87,6 +92,9 @@ def create_app(test_config=None):
             results_lst[tok] = {}
             results_lst[tok]['thread'] = Thread(target = _run, args = (func_call, args, kwargs, app.config['BWT'], tok))
             results_lst[tok]['thread'].start()
+            results_lst[tok]['func'] = func_call
+            results_lst[tok]['args'] = args
+            results_lst[tok]['kwargs'] = kwargs
             #tmp = pool.apply_async(_run, args = (func_call, args, kwargs, app.config['BWT'], tok))
             st = 200
             #except:
@@ -105,9 +113,13 @@ def create_app(test_config=None):
         try:
             token = token.encode('ascii', 'ignore')
             j = results_lst[token]
-            data = {'result': j['result'], 'date': j['date'].strftime("%m/%d/%Y, %H:%M:%S"), 'status': j['status']}
-            if j['done'] and j['status'] == 'SUCCESS':
-                data['result'] = j['result']
+            data = {'result': j['result'], 
+                    'date': j['date'].strftime("%m/%d/%Y, %H:%M:%S"), 
+                    'status': j['status'],
+                    'function': j['func'],
+                    'args': j['args'],
+                    'kwargs': j['kwargs']}
+            
             return Response(json.dumps(data), status = 200)
         except Exception as e:
             print(e)
@@ -119,28 +131,59 @@ def create_app(test_config=None):
         return Response(status=200)
 
     def _run( func_call, args, kwargs, bwt, token):
+
+        """
+            Initializes Job entry in Result List.
+        """
         results_lst[token] = {}
         results_lst[token]['done'] = False
         results_lst[token]['date'] = dt.datetime.now()
         results_lst[token]['result'] = None
         results_lst[token]['status'] = 'RUNNING'
         try:  
+            
             available = dir(bwt)
+            
+            # Handles Basic BWT functions
             if func_call in available:
                 f = getattr(bwt, func_call)
                 result = f(*args, **kwargs)
                 results_lst[token]['result'] = result
-                results_lst[token]['status'] = 'SUCCESS'
+            
+            # Long running job for testing non-blocking properties
             elif func_call == 'testr':
                 x = int(args[0])
                 time.sleep(x)
                 results_lst[token]['result'] = "Slept " + str(x) + " seconds"
-                results_lst[token]['status'] = 'SUCCESS'
-        except Exception as e:
-            print(e)
-            results_lst[token]['status'] = 'FAILED'
+                
+            # Recovers all strings from range of indices, non-parallel
+            elif func_call == 'batchRecoverString':
+                recoverStrings = []
+                for index in range(args[0], args[1] + 1):
+                    recoverStrings.append(bwt.recoverString(index))
+                results_lst[token]['result'] = recoverStrings
+            
+            #Batch Sequence Counts, non parallel
+            elif func_call == 'batchCountOccurrencesOfSeq':
+                counts = []
+                for seq in args:
+                    counts.append(bwt.countOccurrencesOfSeq(seq))
+                results_lst[token]['result'] = counts
 
+            # Optimized Occurrence Count
+            elif func_call == 'batchFastCountOccurrencesOfSeq':
+                results_lst[token]['result'] = fastBatchKmerCounts(bwt, args)
+
+
+        except Exception as e:
+            results_lst[token]['result'] = e.message
+            results_lst[token]['status'] = 'FAILED'
+            results_lst[token]['done'] = True
+            return
+
+        results_lst[token]['status'] = 'SUCCESS'
         results_lst[token]['done'] = True
+        return
 
             
             
